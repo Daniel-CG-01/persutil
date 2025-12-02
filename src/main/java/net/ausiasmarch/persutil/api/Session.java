b586c19f9d6b3a3810eaecb74417cd77189af5ea
DanielZR05 <danielzanon2005@gmail.com>
2025-12-02 11:56:04 +0100

    Revertir últimos commits (incluidos los merge), excepto los de hoy

---
+++ src/main/java/net/ausiasmarch/persutil/api/Session.java
@@ -0,0 +1,24 @@
+package net.ausiasmarch.persutil.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import net.ausiasmarch.persutil.bean.SessionBean;
+import net.ausiasmarch.persutil.service.SessionService;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/session")
+public class Session {
+
+    @Autowired
+    SessionService oSessionService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody SessionBean oSessionBean) {
+        return ResponseEntity.ok(oSessionService.login(oSessionBean));
+    }
+}
