54019490025bf232032e6e583b41c1239ca270c5
DanielZR05 <danielzanon2005@gmail.com>
2025-12-15 12:08:12 +0100

    Ya he importado el "import" que faltaba en JWTService.java. Falta acabar las excepciones

--- src/main/java/net/ausiasmarch/persutil/service/JWTService.java
+++ src/main/java/net/ausiasmarch/persutil/service/JWTService.java
@@ -1,5 +1,6 @@
 package net.ausiasmarch.persutil.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
